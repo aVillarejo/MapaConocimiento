a926c0f4596dd686dab62badd2e52e45776f6caa
“a_villarejo19” <a.villarejo19@gmail.com>
2018-10-28 17:28:35 -0600

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class componentName extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Mapa del Conocimiento</h1>
+      </div>
+    );
+  }
+}
+
+export default componentName;
